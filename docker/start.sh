8bada2591eafeaaa0382595a61fa6715c2ee1e3b
Sergei Linok <serega.linok5555@yandex.ru>
2021-03-30 00:54:04 +0300

    added datasets -v

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+DATASETS=$1
+
 docker run --rm \
 	   -ti \
            -n yolact_edge \
            --gpus all \
-           -v ..:/home/yolact_edge/yolact_edge/:rw
+           -v ..:/home/yolact_edge/yolact_edge/:rw \
+           -v $DATASETS:/datasets/:ro \
            yolact_edge
 
 ./into.sh
