d10ef58d207f17cb934f6f5a74ae73a4a75775d2
linukc <35877675+linukc@users.noreply.github.com>
2021-03-30 01:40:23 +0300

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ DATASETS=$1
 
 docker run --rm \
 	   -ti \
-           -n yolact_edge \
+           --name yolact_edge \
            --gpus all \
            -v ..:/home/yolact_edge/yolact_edge/:rw \
            -v $DATASETS:/datasets/:ro \
