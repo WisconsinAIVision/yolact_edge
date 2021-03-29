43074d8d4fbe97366ff22f056cb720a36cfe17f2
Sergei Linok <serega.linok5555@yandex.ru>
2021-03-30 00:45:56 +0300

    added code source via -v

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm -ti -n yolact_edge --gpus all yolact_edge
+docker run --rm \
+	   -ti \
+           -n yolact_edge \
+           --gpus all \
+           -v ..:/home/yolact_edge/yolact_edge/:rw
+           yolact_edge
+
+./into.sh
