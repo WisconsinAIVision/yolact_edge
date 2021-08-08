f831cf09dd59f120a1051e48a1896556c29cd6eb
saul <saul.goldblat@sagarobotics.com>
2021-08-08 18:55:51 +0100

    modified docker start to open ports for tensorboard

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,8 @@ SOURCE_CODE=$1
 DATASETS=$2
 
 docker run --gpus all -it --name=yolact_edge \
-  --shm-size=8gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+   --shm-size=8gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -p 8082:22 -p 8083:6006 \
   -v $SOURCE_CODE:/yolact_edge/:rw \
   -v $DATASETS:/datasets/:ro \
   yolact_edge_image
